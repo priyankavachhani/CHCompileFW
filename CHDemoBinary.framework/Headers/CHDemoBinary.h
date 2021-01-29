e66c96e5c1094b76e2ee7b52efab5a45a6ea857f
priyankavachhani@callhippo.com <priyankavachhani@callhippo.com>
2021-01-29 17:58:58 +0530

    initial commit

---
+++ CHDemoBinary.framework/Headers/CHDemoBinary.h
@@ -0,0 +1,19 @@
+//
+//  CHDemoBinary.h
+//  CHDemoBinary
+//
+//  Created by Apple on 29/01/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CHDemoBinary.
+FOUNDATION_EXPORT double CHDemoBinaryVersionNumber;
+
+//! Project version string for CHDemoBinary.
+FOUNDATION_EXPORT const unsigned char CHDemoBinaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHDemoBinary/PublicHeader.h>
+
+
